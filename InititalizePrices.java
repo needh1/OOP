a55e1557b1d1c1069fa740b119c273e8eacace47
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2022-11-04 15:46:31 +0800

    created price file initializer

---
+++ InititalizePrices.java
@@ -0,0 +1,38 @@
+//USED TO INITIALIZE THE PRICES, NOT PART OF THE APP!!!
+
+public class InitializePrices
+{
+    public static void main(String[] args){
+        PriceStorage storage = new PriceStorage();
+
+        Price prices = new Price();
+
+        //Set price for student
+        prices.addPrice(PricingType.STUDENT_2D_STANDARD, 7);
+        prices.addPrice(PricingType.STUDENT_2D_PREMIUM, 9);
+        prices.addPrice(PricingType.STUDENT_3D_STANDARD, 8);
+        prices.addPrice(PricingType.STUDENT_3D_PREMIUM, 10);
+
+        //Set price for senior
+        prices.addPrice(PricingType.SENIOR_2D_STANDARD, 5);
+        prices.addPrice(PricingType.SENIOR_2D_PREMIUM, 7);
+        prices.addPrice(PricingType.SENIOR_3D_STANDARD, 6);
+        prices.addPrice(PricingType.SENIOR_3D_PREMIUM, 8);
+
+        //Set price for normal
+        prices.addPrice(PricingType.NORMAL_2D_STANDARD, 8);
+        prices.addPrice(PricingType.NORMAL_2D_PREMIUM, 10);
+        prices.addPrice(PricingType.NORMAL_3D_STANDARD, 9);
+        prices.addPrice(PricingType.NORMAL_3D_PREMIUM, 11);
+        
+        //Set price for Weekend/holidays
+        prices.addPrice(PricingType.WEEKEND_2D_STANDARD, 10);
+        prices.addPrice(PricingType.WEEKEND_2D_PREMIUM, 12);
+        prices.addPrice(PricingType.WEEKEND_3D_STANDARD, 11);
+        prices.addPrice(PricingType.WEEKEND_3D_PREMIUM, 13);
+        
+        
+
+        storage.writeObject(prices);
+    }
+}
