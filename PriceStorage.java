3d284091f64e4ed89cf7000b85e178cbdc31bf1c
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2022-11-04 15:37:23 +0800

    Created PriceStorage.java

---
+++ PriceStorage.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+
+public class PriceStorage extends FileStorage
+{
+    public final static String FILENAME = "prices.txt";
+
+    public void writeObject(Object o) {
+        if(o instanceof Price){
+            Price price = (Price) o;
+            try {
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILENAME));
+                out.writeObject(price);
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Price read(){
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILENAME));   
+            Price prices = (Price) ois.readObject();
+            ois.close();
+            return prices;
+        } catch (ClassNotFoundException e) {
+            System.out.println("Object does not exist.");
+        } catch (IOException e){
+
+        }
+        return new Price();
+    }
+}
