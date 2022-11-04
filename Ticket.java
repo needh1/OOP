6dd1811f676a46ded4423109102e61e1c886cd1d
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2022-11-05 01:20:53 +0800

    added Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,64 @@
+import java.io.Serializable;
+import java.time.*;
+
+public class Ticket implements Serializable
+{
+    private String ticketID;
+    private String movieName;
+    private MovieType movieType;
+    private CinemaType cinemaType;
+    private double price;
+    private int seatNo;
+    private LocalDate showDate;
+    private LocalTime showTime;
+    private String cineplexName;
+
+    public Ticket(String ticketID, String movieName, MovieType type, CinemaType cinemaType, double price, int seatNo,
+                    LocalDate showDate, LocalTime showTime, String cineplexName){
+        this.ticketID = ticketID;
+        this.movieName = movieName;
+        this.movieType = type;
+        this.cinemaType = cinemaType;
+        this.price = price;
+        this.seatNo = seatNo;
+        this.showDate = showDate;
+        this.showTime = showTime;
+        this.cineplexName = cineplexName;
+    }
+
+    public String getTicketID(){
+        return ticketID;
+    }
+
+    public String getMovieName(){
+        return movieName;
+    }
+
+    public MovieType getMovieType(){
+        return movieType;
+    }
+
+    public CinemaType getCinemaType(){
+        return cinemaType;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public int getSeatNo(){
+        return seatNo;
+    }
+
+    public LocalDate getDate(){
+        return showDate;
+    }
+
+    public LocalTime getTime(){
+        return showTime;
+    }
+
+    public String getCineplexName(){
+        return cineplexName;
+    }
+}
